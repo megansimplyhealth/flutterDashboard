ddcaa87537cd90504a7420d5a65b49c69a8feda5
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-10-07 11:47:10 +0100

    key add

--- lib/Screens/dashboardHome.dart
+++ lib/Screens/dashboardHome.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DashboardHome extends StatelessWidget {
+  const DashboardHome({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
